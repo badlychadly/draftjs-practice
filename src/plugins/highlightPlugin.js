93a9929ad28bd37dd88c81dec11c7dd9e7d3a314
badlychadly <chadmontoya21@yahoo.com>
2019-06-21 20:30:40 -0700

    make highlight look like code snippet

--- src/plugins/highlightPlugin.js
+++ src/plugins/highlightPlugin.js
@@ -1,11 +1,21 @@
 import { RichUtils, getDefaultKeyBinding, KeyBindingUtil} from 'draft-js'
 const {hasCommandModifier} = KeyBindingUtil;
 
+// background: "rgba(27,31,35,.05)",
+// 				border-radius: "3px",
+// 				font-size: "85%",
+// 				margin: "0",
+// 				padding: ".2em .4em"
+
 export default () => {
 	return {
 		customStyleMap: {
 			HIGHLIGHT: {
-				background: "#fffe0d"
+				background: "rgba(0,0,0,.05)",
+				"border-radius": "3px",
+				"font-size": "85%",
+				margin: "0",
+				padding: ".2em .4em"
 			}
 		},
 		keyBindingFn: e => {
